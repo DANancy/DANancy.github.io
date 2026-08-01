a452b5f424426286eb829141079968abce9f6cdf
DANancy <gnancy417@gmail.com>
2026-08-01 16:08:25 +1000

    Add magical study start screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { MagicalWorld } from "@/components/world/MagicalWorld";
+import { MagicalStart } from "@/components/world/MagicalStart";
 import "@/components/world/world.css";
+import "@/components/world/start.css";
 
 export default function HomePage() {
-  return <MagicalWorld />;
+  return <MagicalStart />;
 }
