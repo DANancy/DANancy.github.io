2dcc84e15e7eb16f943449b21b22ca847cc573d5
DANancy <gnancy417@gmail.com>
2026-08-06 20:13:30 +1000

    Update portfolio community and learning content

--- src/app/zh-hans/layout.tsx
+++ src/app/zh-hans/layout.tsx
@@ -1,5 +1,3 @@
-import Script from "next/script";
-
 export default function ChineseLayout({children}:{children:React.ReactNode}){
-  return <><Script id="set-chinese-document-language" strategy="beforeInteractive">{`document.documentElement.lang="zh-CN";`}</Script>{children}</>;
+  return <div lang="zh-CN">{children}</div>;
 }
